867edc4d99989c2a652d7d353ddb60b196f4da23
Vlad Taranov <wvlad.dev@gmail.com>
2017-02-18 00:43:43 +0200

    extension factory method for Ref<T>

---
+++ Aqla.DtoHelpers/DtoHelpersExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Aqla.DtoHelpers
+{
+    public static class DtoHelpersExtensions
+    {
+        public static Ref<T> CloneAndMakeRef<T>(this T value) where T : struct
+        {
+            return new Ref<T>(value);
+        }
+    }
+}

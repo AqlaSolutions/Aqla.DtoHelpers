91ff6457b88a0312222a6b21b13f451beb4f7bb2
Vlad Taranov <wvlad.dev@gmail.com>
2017-02-17 11:31:16 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoHelpers
+{
+    public class Class1
+    {
+    }
+}

267fb60f66a831d4f55923b242e9b77f264dbe43
Vlad Taranov <wvlad.dev@gmail.com>
2017-02-17 13:23:07 +0200

    amend

--- Aqla.DtoHelpers/Ref.cs
+++ Aqla.DtoHelpers/Ref.cs
@@ -2,6 +2,10 @@
 
 namespace Aqla.DtoHelpers
 {
+    /// <summary>
+    /// Writable struct by ref, don't use for nested
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Ref<T>
         where T : struct
     {
